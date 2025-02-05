61e44ef419db6fbc88a259360da8f7e87cecdef9
mathiassol <114577027+mathiassol@users.noreply.github.com>
2025-02-05 09:20:50 +0100

    1

--- resizeController.js
+++ resizeController.js
@@ -1,28 +1,53 @@
-var isResizing = false,
-    lastDownX = 0;
+const container = document.getElementById("container");
+const leftPanel = document.getElementById("left_panel");
+const rightPanel = document.getElementById("right_panel");
+const drag = document.getElementById("drag");
 
-$(function () {
-    var container = $('#container'),
-        left = $('#left_panel'),
-        right = $('#right_panel'),
-        handle = $('#drag');
+let isResizing = false;
 
-    handle.on('mousedown', function (e) {
-        isResizing = true;
-        lastDownX = e.clientX;
-    });
+function getStoredPosition() {
+    let ratio = parseFloat(localStorage.getItem("panelPosition"));
+    return isNaN(ratio) ? 0.2 : ratio;
+}
+function savePosition(ratio) {
+    localStorage.setItem("panelPosition", ratio);
+}
+function updatePanelSize(ratio) {
+    let containerWidth = container.clientWidth;
+    let minWidth = containerWidth * 0.15;
+    let maxWidth = containerWidth * 0.5;
+    let leftWidth = minWidth + ratio * (maxWidth - minWidth);
 
-    $(document).on('mousemove', function (e) {
-        // we don't want to do anything if we aren't resizing.
-        if (!isResizing)
-            return;
+    leftPanel.style.width = leftWidth + "px";
+    rightPanel.style.width = containerWidth - leftWidth - drag.clientWidth + "px";
+}
+function resize(e) {
+    if (!isResizing) return;
 
-        var offsetRight = container.width() - (e.clientX - container.offset().left);
+    let containerWidth = container.clientWidth;
+    let minWidth = containerWidth * 0.15;
+    let maxWidth = containerWidth * 0.5;
+    let leftWidth = Math.max(minWidth, Math.min(e.clientX, maxWidth));
 
-        left.css('right', offsetRight);
-        right.css('width', offsetRight);
-    }).on('mouseup', function (e) {
-        // stop resizing
-        isResizing = false;
-    });
+    let ratio = (leftWidth - minWidth) / (maxWidth - minWidth);
+    savePosition(ratio);
+    updatePanelSize(ratio);
+}
+function stopResize() {
+    isResizing = false;
+    document.removeEventListener("mousemove", resize);
+    document.removeEventListener("mouseup", stopResize);
+}
+updatePanelSize(getStoredPosition());
+
+drag.addEventListener("mousedown", (e) => {
+    isResizing = true;
+    document.addEventListener("mousemove", resize);
+    document.addEventListener("mouseup", stopResize);
+});
+document.addEventListener("fullscreenchange", () => {
+    updatePanelSize(getStoredPosition());
+});
+window.addEventListener("resize", () => {
+    updatePanelSize(getStoredPosition());
 });
